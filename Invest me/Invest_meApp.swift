def0dd3a290382dc1969fe7f81d347f0c104aef0
marufkhonovismatillo <marufkhonovismatillo@gmail.com>
2022-05-29 11:18:34 +0500

    Initial Commit

---
+++ Invest me/Invest_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  Invest_meApp.swift
+//  Invest me
+//
+//  Created by Ismatillo Marufkhonov on 29/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Invest_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
